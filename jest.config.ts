2328abba9d79e08e463827cf424544b6f771923d
MUNTHER ABDELRAZEQ ALOUFI <maaloufi20@cit.just.edu.jo>
2024-10-26 21:49:36 +0300

    Feat: Setup test with jest

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+// jest.config.ts
+import type { Config } from 'jest';
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const config: Config = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/*.stories.{js,jsx,ts,tsx}',
+    '!src/app/layout.tsx',
+  ],
+};
+
+export default createJestConfig(config);
